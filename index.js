a55c80f7e0496f368ff6b509d9fd0baef0e6956f
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-10-18 17:48:38 -0400

    initialize variable for buttonReset + add event listener reset click

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const buttonText = document.querySelector("button");
 
 const buttonClick = document.querySelector(".click");
 buttonClick.addEventListener("click");
+
+const buttonReset = document.querySelector(".reset")
+button.addEventListener("click");
