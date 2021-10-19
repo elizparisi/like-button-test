2b39f3298514d183a34daf5b7829789582ea74de
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-10-18 20:23:22 -0400

    function to likePost + add likes

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ buttonClick.addEventListener("click");
 
 const buttonReset = document.querySelector(".reset")
 button.addEventListener("click");
+
+
+let numOfLikes = 0;
+
+function likePost() {
+  numOfLikes = numOfLikes + 1;
+
+  if (numOfLikes === 1){
+    buttonText.innerHTML = ("1 Like");
+  }
+  else {
+    buttonText.innerHTML = (numOfLikes + " " + "Likes")
+  }
+}
