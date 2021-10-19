04679176db3fbab2810c1ba2d273fbeead28f0c5
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-10-18 21:22:49 -0400

    initialize counter variable + set initial count to 0;

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const buttonText = document.querySelector("button");
-
-const buttonClick = document.querySelector(".click");
-buttonClick.addEventListener("click", likePost);
-
-const buttonReset = document.querySelector(".reset")
-button.addEventListener("click");
+let counter = document.querySelector("#counter");
+let count = 0;
 
 
-let numOfLikes = 0;
-
-function likePost() {
-  numOfLikes = numOfLikes + 1;
-
-  if (numOfLikes === 1){
-    buttonText.innerHTML = ("1 Like");
-  }
-  else {
-    buttonText.innerHTML = (numOfLikes + " " + "Likes")
-  }
-}
-
-function resetLikes() {
-  numOfLikes = 0;
-
-  buttonText.innerHTML = numOfLikes + "Likes";
-}
+// const buttonText = document.querySelector("button");
+//
+// const buttonClick = document.querySelector(.click);
+// buttonClick.addEventListener("click", likePost);
+//
+// const buttonReset = document.querySelector(.reset)
+// button.addEventListener("click", resetLikes);
+//
+//
+// let numOfLikes = 0;
+//
+// function likePost() {
+//   numOfLikes = numOfLikes + 1;
+//
+//   if (numOfLikes === 1){
+//     buttonText.innerHTML = ("1 Like");
+//   }
+//   else {
+//     buttonText.innerHTML = (numOfLikes + " " + "Likes")
+//   }
+// }
+//
+// function resetLikes() {
+//   numOfLikes = 0;
+//
+//   buttonText.innerHTML = numOfLikes + "Likes";
+// }
